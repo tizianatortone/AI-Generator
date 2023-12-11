6deaf46e1f97ae82ffaaa440a22727461e5150e7
tizianatortone <tiziana_tortone@hotmail.com>
2023-12-11 15:08:24 +0100

    basic form js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: ["La tombe dit a la rose"],
+    autoStart: true,
+    delay: 1,
+    cursor: null,
+  });
+}
+
+let inputFormElement = document.querySelector("#AI-generator-form");
+inputFormElement.addEventListener("submit", generatePoem);
